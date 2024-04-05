ea42438a599e75ed8f2f3adf5322cddd1370ed5f
ajkaitran <trannamhai7626@gmail.com>
2024-04-05 09:25:04 +0700

    upoder

--- views/home/product.php
+++ views/home/product.php
@@ -3,8 +3,7 @@
         <div class="container">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><i class="fa-duotone fa-house-chimney me-3"></i><a class="p__hover"
-                            href="#">Trang chủ</a></li>
+                    <li class="breadcrumb-item"><i class="fa-duotone fa-house-chimney me-3"></i><a class="p__hover" href="#">Trang chủ</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Sản phẩm</li>
                 </ol>
             </nav>
@@ -23,125 +22,30 @@
                                 <?php
                                 foreach ($categories as $cate) {
                                 ?>
-<<<<<<< HEAD
                                     <li>
                                         <div class="border__dashed p__hover">
-                                            <a class="p__hover" href="#"><?= $cate['parent']['Name'] ?></a>
-                                            <?php
-                                            if (!empty($cate['children'])) {
-                                                echo '<i class="fa-regular fa-caret-down dropdown"></i>';
-                                            }
-                                            ?>
+                                            <a class="p__hover" href="?controller=home&action=ProductCategory&Id=<?= $cate['Id'] ?>"><?= $cate['Name'] ?></a>
+                                            <i class="fa-regular fa-caret-down dropdown"></i>
                                         </div>
-                                        <ul class="dropdown__menu ps-2">
-                                            <?php
-
-                                            foreach ($cate['children'] as $key => $value) :
-                                            ?>
-                                                <li class="border__solid">
-                                                    <a class="p__hover" href="#"><?= $value['Name'] ?></a>
-                                                </li>
-                                            <?php endforeach; ?>
-                                        </ul>
                                     </li>
-=======
-                                <li>
-                                    <div class="border__dashed p__hover">
-                                        <a class="p__hover"
-                                            href="?controller=home&action=ProductCategory&Id=<?= $cate['Id'] ?>"><?= $cate['Name']?></a>
-                                        <i class="fa-regular fa-caret-down dropdown"></i>
-                                    </div>
-                                </li>
->>>>>>> origin/dinh0107
                                 <?php
                                 }
                                 ?>
                             </ul>
                         </div>
                     </div>
-<<<<<<< HEAD
-                    <div class="sidebar">
-                        <div class="sidebar__title">
-                            <h4 class="h4__white">TÌM THEO GIÁ</h4>
-                        </div>
-                        <div class="sidebar__range__slider">
-                            <input type="text" class="js-range-slider" id="priceRange" name="my_range" value="" />
-                        </div>
-                    </div>
-                    <div class="sidebar">
-                        <div class="sidebar__title">
-                            <h4 class="h4__white">SẢN PHẨM HOT</h4>
-                        </div>
-                        <div class="slide__favorite__product__sidebar py-3">
-                            <?php
-                            foreach ($listProduct as $key => $item) {
-                            ?>
-                                <div class="box__favorite__product">
-                                    <a href="?controller=home&action=ProductDetail">
-                                        <img src="./public/uploads/AnhSanPham/<?= $item['Image'] ?>" alt="">
-                                        <p class="p__hover"><?= $item['Name'] ?></p>
-                                        <div class="price">
-                                            <h5 class="new__price"><?= number_format($item['PriceSale']) ?>VND</h5>
-                                            <del class="old__price p__hover"><?= number_format($item['Price']) ?>VND</del>
-                                        </div>
-                                    </a>
-                                    <div class="add__to__cart" onclick="QuickCart(id)"><i class="fa-regular fa-bag-shopping fs-5"></i>Thêm vào giỏ hàng</div>
-                                    <div class="floating__box__product">
-                                        <div class="box__product">
-                                            <a asp-action="ProductView" asp-controller="Home" asp-route-id="@item.Id" data-fancybox="" data-type="ajax" class="item-product">
-                                                <i class="fa-regular fa-eye"></i>
-                                            </a>
-                                        </div>
-                                        <div class="box__product">
-                                            <a href="javascript:;" onclick="AddToWishlist('@item.Id')"><i class="fa-regular fa-heart"></i></a>
-                                        </div>
-                                        <div class="box__product">
-                                            <a href="javascript:;" onclick="AddToCollation('@item.Id')"><i class="fa-solid fa-code-compare fa-rotate-90"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php
-                            }
-                            ?>
-                        </div>
-                    </div>
-=======
->>>>>>> origin/dinh0107
                 </div>
                 <div class="col-lg-9 order-0 order-lg-1 ">
                     <div class="display__product">
                         <div class="row">
                             <div class="col-lg-12 col-md-7 col-sm-12">
                                 <label>Hiển thị</label>
-<<<<<<< HEAD
-                                <select>
-                                    <option>Mặc định</option>
-                                    <option>12</option>
-                                    <option>24</option>
-                                    <option>36</option>
-                                    <option>48</option>
-                                    <option>60</option>
-                                </select>
-                            </div>
-                            <div class="col-lg-7 col-md-7 col-sm-12">
-                                <label>Sắp xếp</label>
-                                <select>
-                                    <option>Mặc định</option>
-                                    <option>Sấp xếp theo tên (A-Z)</option>
-                                    <option>Sấp xếp theo tên (Z-A)</option>
-                                    <option>Sấp xếp theo giá (Nhỏ-Lớn)</option>
-                                    <option>Sấp xếp theo giá (Lớn-Nhỏ)</option>
-                                    <option>Sấp xếp theo khuyến mãi (Có-Không)</option>
-                                    <option>Sấp xếp theo khuyến mãi (Không-Có)</option>
-=======
-                                <select class="form-control mt-2" name="Sort"
-                                    onchange="window.location.href='?controller=home&action=product&Sort='+this.value">
+                                <select class="form-control mt-2" name="Sort" onchange="window.location.href='?controller=home&action=product&Sort='+this.value">
                                     <option value="">Mặc định</option>
                                     <option <?= 1 == $Sort ? "selected" : null ?> value="1">Sắp xếp theo tên (A-Z)</option>
                                     <option <?= 2 == $Sort ? "selected" : null ?> value="2">Sắp xếp theo tên (Z-A)</option>
                                     <option <?= 3 == $Sort ? "selected" : null ?> value="3">Sắp xếp theo giá (Nhỏ-Lớn)</option>
                                     <option <?= 4 == $Sort ? "selected" : null ?> value="4">Sắp xếp theo giá (Lớn-Nhỏ)</option>
->>>>>>> origin/dinh0107
                                 </select>
                             </div>
                         </div>
@@ -151,37 +55,32 @@
                             <?php
                             foreach ($listProduct as $key => $item) {
                             ?>
-                            <div class="col">
-                                <div class="box__favorite__product">
-                                    <a href="?controller=home&action=ProductDetail&Id=<?= $item['Id'] ?>">
-                                        <img src="./public/uploads/AnhSanPham/<?= $item['Image'] ?>" alt="">
-                                        <p class="p__hover"><?= $item['Name'] ?></p>
-                                        <div class="price d-flex" style="gap:10px">
-                                            <h5 class="new__price"><?= number_format($item['PriceSale']) ?>VND</h5>
-                                            <del
-                                                class="old__price p__hover"><?= number_format($item['Price']) ?>VND</del>
-                                        </div>
-                                    </a>
-                                    <div class="add__to__cart" onclick="QuickCart(id)"><i
-                                            class="fa-regular fa-bag-shopping fs-5"></i>Thêm vào giỏ hàng</div>
-                                    <div class="floating__box__product">
-                                        <div class="box__product">
-                                            <a asp-action="ProductView" asp-controller="Home" asp-route-id="@item.Id"
-                                                data-fancybox="" data-type="ajax" class="item-product">
-                                                <i class="fa-regular fa-eye"></i>
-                                            </a>
-                                        </div>
-                                        <div class="box__product">
-                                            <a href="javascript:;" onclick="AddToWishlist('@item.Id')"><i
-                                                    class="fa-regular fa-heart"></i></a>
-                                        </div>
-                                        <div class="box__product">
-                                            <a href="javascript:;" onclick="AddToCollation('@item.Id')"><i
-                                                    class="fa-solid fa-code-compare fa-rotate-90"></i></a>
+                                <div class="col">
+                                    <div class="box__favorite__product">
+                                        <a href="?controller=home&action=ProductDetail&Id=<?= $item['Id'] ?>">
+                                            <img src="./public/uploads/AnhSanPham/<?= $item['Image'] ?>" alt="">
+                                            <p class="p__hover"><?= $item['Name'] ?></p>
+                                            <div class="price d-flex" style="gap:10px">
+                                                <h5 class="new__price"><?= number_format($item['PriceSale']) ?>VND</h5>
+                                                <del class="old__price p__hover"><?= number_format($item['Price']) ?>VND</del>
+                                            </div>
+                                        </a>
+                                        <div class="add__to__cart" onclick="QuickCart(id)"><i class="fa-regular fa-bag-shopping fs-5"></i>Thêm vào giỏ hàng</div>
+                                        <div class="floating__box__product">
+                                            <div class="box__product">
+                                                <a asp-action="ProductView" asp-controller="Home" asp-route-id="@item.Id" data-fancybox="" data-type="ajax" class="item-product">
+                                                    <i class="fa-regular fa-eye"></i>
+                                                </a>
+                                            </div>
+                                            <div class="box__product">
+                                                <a href="javascript:;"><i class="fa-regular fa-heart"></i></a>
+                                            </div>
+                                            <div class="box__product">
+                                                <a href="javascript:;"><i class="fa-solid fa-code-compare fa-rotate-90"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
                             <?php
                             }
                             ?>
